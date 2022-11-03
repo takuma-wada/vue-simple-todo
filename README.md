07b4b98e41190c960b32d37d039af9dc8456ed93
takuma-wada <9th.takuma@gmail.com>
2022-11-03 18:02:18 +0900

    vue-simple-todo

--- README.md
+++ README.md
@@ -1,24 +1,2 @@
-# vue3_practice
-
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+vue練習用
+- 追加 削除 完了 だけの機能
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <body>
+    <div id="app">
+      <h2>TODO List</h2>
+      <form v-on:submit.prevent>
+        <input type="text" v-model="newItem">
+        <button v-on:click="addItem">Add</button>
+      </form>
+      <ul>
+        <li v-for="todo, index in todos" :key="todo.item">
+          <input type="checkbox" v-model=todo.isDone>
+          <span v-bind:class="{done: todo.isDone}">
+            {{ todo.item }}
+          </span>
+          <button v-on:click="deleteItem(index)">削除</button>
+        </li>
+      </ul>
+    </div>
+  </body>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    'newItem': '',
+    'todos': []
+  }),
+  methods: {
+    addItem() {
+      if (this.newItem==='') return
+      let todo = {
+        item: this.newItem,
+        isDone: false
+      }
+      this.todos.push(todo)
+      this.newItem = ''
+    },
+    deleteItem(index) {
+      console.log(index)
+      this.todos.splice(index, 1)
+    }
+  },
+}
+</script>
+
+<style scoped>
+  li {
+    list-style: none;
+  }
+  .done{
+    text-decoration: line-through;
+  }
+</style>
